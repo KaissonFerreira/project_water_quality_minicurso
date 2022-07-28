2f1274058b4fa4ce80a74a0be24e44735932e123
KaissonFerreira <kaysson_17@hotmail.com>
2022-07-28 10:11:29 -0300

    Insert model

--- app.py
+++ app.py
@@ -8,9 +8,26 @@ import streamlit as st
 # Diretórios
 PACKAGE_ROOT = pathlib.Path('.').resolve()
 MODEL_DIR = os.path.join(PACKAGE_ROOT, 'model')
-FILE_MODEL = os.path.join(MODEL_DIR,'clf_randomforest.joblib')
+FILE_MODEL_RF = os.path.join(MODEL_DIR,'clf_randomforest.joblib')
+FILE_MODEL_LR = os.path.join(MODEL_DIR,'clf_logisticregression.joblib')
 
 
 # Carregando o modelo
-model = joblib.load(FILE_MODEL)
+model_rf = joblib.load(FILE_MODEL_RF)
+model_lr = joblib.load(FILE_MODEL_LR)
+
+
+st.set_page_config(
+    page_title="Kaisson Ferreira", page_icon=":round_pushpin:", initial_sidebar_state="expanded"
+                )
+
+st.title('APP - Modelo de classificação')
+
+
+st.write("""
+# :bar_chart: Projeto de Data Science
+
+Predição da qualidade da água
+
+""")
 
